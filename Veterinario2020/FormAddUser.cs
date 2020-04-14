22726e76d0610b3fa1fc26fdbf0f8f6ca5611128
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-04-14 17:07:08 +0200

    Retoques inserción de usuarios

--- Veterinario2020/FormAddUser.cs
+++ Veterinario2020/FormAddUser.cs
@@ -24,13 +24,28 @@ namespace Veterinario2020
         //Inserta el usuario creado en la base de dados
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBoxNombre.Text.Length>1 && textBoxNombre.Text.Length>1 && textBoxdni.Text.Length>5 && textBoxDir.Text.Length>5 && textBoxPass.Text.Length>3)
+            if (textBoxNombre.Text.Length>1 && textBoxApellido.Text.Length>1 && textBoxdni.Text.Length>5 && textBoxDir.Text.Length>5 && textBoxPass.Text.Length>3)
             {
                 string myHash = BCrypt.Net.BCrypt.HashPassword(textBoxPass.Text, BCrypt.Net.BCrypt.GenerateSalt());
 
                 
-                c.addUser(textBoxNombre.Text, textBoxApellido.Text, textBoxdni.Text,textBoxEmail.Text,textBoxDir.Text, textBoxTelefono.Text, myHash);
-                //tablaUsuarios.DataSource = c.obtenerDatos("SELECT dni AS DNI, nombre AS Nombre, apellido AS Apellido, email AS Email, telefono AS Telefono FROM usuario WHERE administrador=FALSE; ");//Actualizamos la tabala con el nuevo usuario
+                c.addUser(textBoxNombre.Text, textBoxApellido.Text, textBoxdni.Text.ToUpper(),textBoxEmail.Text,textBoxDir.Text, textBoxTelefono.Text, myHash);
+                tablaUsuarios.DataSource = c.obtenerDatos("SELECT dni AS DNI, nombre AS Nombre, apellido AS Apellido, email AS Email, telefono AS Telefono FROM usuario WHERE administrador=FALSE; ");//Actualizamos la tabala con el nuevo usuario
+                MessageBox.Show("Usuario añadido ", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //Vacíamos los datos introducidos anteriormente
+
+                textBoxNombre.Text = "";
+                textBoxApellido.Text = "";
+                textBoxdni.Text = "";
+                textBoxDir.Text = "";
+                textBoxTelefono.Text = "";
+                textBoxPass.Text = "";
+
+            }
+            else
+            {
+                MessageBox.Show("Datos no válidos", "Información", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
             
